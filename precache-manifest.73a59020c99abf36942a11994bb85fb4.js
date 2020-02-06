89832449b7a72285832df0371e15804cda42f514
BohdanRadchenko <radchenkobs@gmail.com>
2020-02-06 10:55:47 +0200

    Updates

---
+++ precache-manifest.73a59020c99abf36942a11994bb85fb4.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f033797776b4160e5af875e85b71829e",
+    "url": "/BohdanRadchenko/sw_test_case/index.html"
+  },
+  {
+    "revision": "f16094983b54ddbcac85",
+    "url": "/BohdanRadchenko/sw_test_case/static/css/10.5758f754.chunk.css"
+  },
+  {
+    "revision": "19f1fb7844ce888a52c2",
+    "url": "/BohdanRadchenko/sw_test_case/static/css/9.9564a526.chunk.css"
+  },
+  {
+    "revision": "baa9dcc447ba8fd58633",
+    "url": "/BohdanRadchenko/sw_test_case/static/css/main.ef4a0b3b.chunk.css"
+  },
+  {
+    "revision": "f16094983b54ddbcac85",
+    "url": "/BohdanRadchenko/sw_test_case/static/js/10.3ad600eb.chunk.js"
+  },
+  {
+    "revision": "df17b7675bb7e4429508",
+    "url": "/BohdanRadchenko/sw_test_case/static/js/11.cfde3249.chunk.js"
+  },
+  {
+    "revision": "90d6cb37c436e3cf6c50",
+    "url": "/BohdanRadchenko/sw_test_case/static/js/12.cbd4435f.chunk.js"
+  },
+  {
+    "revision": "e095649c698bb2c59b6f",
+    "url": "/BohdanRadchenko/sw_test_case/static/js/13.c652d1f7.chunk.js"
+  },
+  {
+    "revision": "17ad13cb9aaa47d41a9b",
+    "url": "/BohdanRadchenko/sw_test_case/static/js/14.2ac80563.chunk.js"
+  },
+  {
+    "revision": "bd3ebbd9f572b5f3f26f",
+    "url": "/BohdanRadchenko/sw_test_case/static/js/2.7b3cedca.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/BohdanRadchenko/sw_test_case/static/js/2.7b3cedca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52aa96e9cf5ca39ad160",
+    "url": "/BohdanRadchenko/sw_test_case/static/js/3.2d09e22b.chunk.js"
+  },
+  {
+    "revision": "df49521bdbf04a4aeedc",
+    "url": "/BohdanRadchenko/sw_test_case/static/js/4.65835c43.chunk.js"
+  },
+  {
+    "revision": "7c417ee62b82df62f70e",
+    "url": "/BohdanRadchenko/sw_test_case/static/js/5.43511c16.chunk.js"
+  },
+  {
+    "revision": "9e59a12627a5fc3fe293",
+    "url": "/BohdanRadchenko/sw_test_case/static/js/6.87ade9c3.chunk.js"
+  },
+  {
+    "revision": "7daba73819b5d5855d62",
+    "url": "/BohdanRadchenko/sw_test_case/static/js/7.60622bec.chunk.js"
+  },
+  {
+    "revision": "2cff6fc206318ca1ad39",
+    "url": "/BohdanRadchenko/sw_test_case/static/js/8.d943a98f.chunk.js"
+  },
+  {
+    "revision": "19f1fb7844ce888a52c2",
+    "url": "/BohdanRadchenko/sw_test_case/static/js/9.63ac7f5e.chunk.js"
+  },
+  {
+    "revision": "baa9dcc447ba8fd58633",
+    "url": "/BohdanRadchenko/sw_test_case/static/js/main.d7f2a094.chunk.js"
+  },
+  {
+    "revision": "711ba7bfc026fff557a0",
+    "url": "/BohdanRadchenko/sw_test_case/static/js/runtime-main.c9982cd1.js"
+  }
+]);
